3d67b85ae648283df9d9f28f9b39d0093709d5d7
Sumit Kumar <vk0922763@gmail.com>
2025-09-12 17:35:57 +0530

    detail api

--- Src/Controllers/Products.controller.js
+++ Src/Controllers/Products.controller.js
@@ -10,15 +10,14 @@ exports.getAllProducts = async (req, res) => {
                 process.env.Encryption_Decryption_Key,
                 JSON.stringify(errorResponse)
             );
-
             return res.status(404).json({ data: encryptedError });
         }
+
         const successResponse = ApiResponse.success(
             Products,
             "All Products Get Successfully!",
             200
         );
-
         // Encrypt the success response
         const encryptedSuccess = await ApiEncryptDecrypt.encryptString(
             process.env.Encryption_Decryption_Key,
@@ -29,16 +28,27 @@ exports.getAllProducts = async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        res.status(500).json(ApiResponse.error("Internal server error"));
+        const errorResponse = ApiResponse.error("Internal server error", 500);
+        res.status(500).json({ data: errorResponse });
     }
 };
-exports.getProductById = (req, res) => {
+exports.getProductById = async (req, res) => {
     const productId = parseInt(req.params.id, 10);
     // Simulating a database call to get a product by ID
     const product = Products.find(p => p.id === productId);
     if (!product) {
-        return res.status(404).json(ApiResponse.error("products not found !"));
+        const errrorResponse = ApiResponse.error("products not found !", 500);
+        const errrorResponse1 = await ApiEncryptDecrypt.encryptString(
+            process.env.Encryption_Decryption_Key,
+            JSON.stringify(errrorResponse)
+        );
+        return res.status(400).json({ data: errrorResponse1 });
     }
     // Sending the product as a response
-    res.status(200).json(ApiResponse.success(product, "Products Get Successfully !"));
+    const successResponse = ApiResponse.success(product, "Products Get Successfully!", 404);
+    const successResponse1 = await ApiEncryptDecrypt.encryptString(
+        process.env.Encryption_Decryption_Key,
+        JSON.stringify(successResponse)
+    );
+    return res.status(200).json({ data: successResponse1 });
 }
